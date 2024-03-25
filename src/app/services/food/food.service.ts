80f18a934b7dc079c262148a2b9364ae8f28b3b3
unknown <lokeshguthi@gmail.com>
2024-03-25 23:31:30 +0100

    added food details to the menu

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Food} from 'src/app/shared/models/Food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,61 @@ export class FoodService {
 
   constructor() { }
 
-  getAll(): String[]{
+  getAll(): Food[]{
     return[
-      '/assets/pictures/foods/1.jpg',
-      '/assets/pictures/foods/2.jpg',
-      '/assets/pictures/foods/3.jpg',
-      '/assets/pictures/foods/4.jpg',
-      '/assets/pictures/foods/5.jpg'
+      {
+        id:1,
+        name:'Dosa',
+        cookTime:'5',
+      price: 10,
+      favourite:false,
+      stars:4.5,
+      imageUrl: '/assets/pictures/foods/1.jpg',
+      origins:['India'],
+
+    },
+    {
+      id:1,
+      name:'Chicken Biryani',
+      cookTime:'20',
+      price: 15,
+      favourite:true,
+      stars:5,
+      imageUrl: '/assets/pictures/foods/2.jpg',
+      origins:['India'],
+    },
+    {
+      id:1,
+      name:'Chicken Fry',
+      cookTime:'20',
+      price: 12,
+      favourite:false,
+      stars:4.5,
+      imageUrl: '/assets/pictures/foods/3.jpg',
+      origins:['India'],
+      
+    },
+    {
+      id:1,
+      name:'Paneer Curry',
+      cookTime:'15',
+      price: 10,
+      favourite:false,
+      stars:4,
+      imageUrl: '/assets/pictures/foods/4.jpg',
+      origins:['India'],
+      
+    },
+    {
+      id:1,
+      name:'Pani Puri',
+      cookTime:'6',
+      price: 8,
+      favourite:true,
+      stars:4.5,
+      imageUrl: '/assets/pictures/foods/5.jpg',
+      origins:['India'],
+    }
     ]
   }
 
